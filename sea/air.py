98c18a820c1f55401892d3ea408adeb57fec041d
Murilo Cardoso Soares <58701063+MuriloCardosoSoares@users.noreply.github.com>
2020-11-06 16:04:59 -0300

    Update air.py

--- sea/air.py
+++ sea/air.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import time, sys
 
 class AirProperties():
+    
     def __init__(self, c0 = 343.0, rho0 = 1.21, temperature = 20.0, humid = 50.0, p_atm = 101325.0):
         '''
         Set up air properties
@@ -14,11 +15,41 @@ class AirProperties():
             humid - relative humidity (default 50 %)
             p_atm - atmospheric pressure (default 101325.0 Pa)
         '''
-        # config = load_cfg(config_file)
-        # self.c0 = np.array(c0, dtype = np.float32)
-        # self.rho0 = np.array(rho0, dtype = np.float32)
+        
         self.c0 = np.array(c0)
         self.rho0 = np.array(rho0)
         self.temperature = np.array(temperature, dtype = np.float32)
         self.hr = np.array(humid, dtype = np.float32)
         self.p_atm = np.array(p_atm, dtype = np.float32)
+        
+     def standardized_c0_rho0(self,):
+        '''
+        This method is used to calculate the standardized value of the sound speed and
+        air density based on measurements of temperature, humidity and atm pressure.
+        It will overwrite the user supplied values
+        '''
+        # kappla = 0.026
+        temp_kelvin = self.temperature + 273.16 # temperature in [K]
+        R = 287.031                 # gas constant
+        rvp = 461.521               # gas constant for water vapor
+        # pvp from Pierce Acoustics 1955 - pag. 555
+        pvp = 0.0658 * temp_kelvin**3 - 53.7558 * temp_kelvin**2 \
+            + 14703.8127 * temp_kelvin - 1345485.0465
+        # Air viscosity
+        # vis = 7.72488e-8 * temp_kelvin - 5.95238e-11 * temp_kelvin**2
+        # + 2.71368e-14 * temp_kelvin**3
+        # Constant pressure specific heat
+        cp = 4168.8 * (0.249679 - 7.55179e-5 * temp_kelvin \
+            + 1.69194e-7 * temp_kelvin**2 \
+            - 6.46128e-11 * temp_kelvin**3)
+        cv = cp - R                 # Constant volume specific heat
+        # b2 = vis * cp / kappla      # Prandtl number
+        gam = cp / cv               # specific heat constant ratio
+        # Air density
+        self.rho0 = self.p_atm / (R * temp_kelvin) \
+            - (1/R - 1/rvp) * self.hr/100 * pvp/temp_kelvin
+        # Air sound speed
+        self.c0 = (gam * self.p_atm/self.rho0)**0.5
+    
+    def __str__(self):
+        return "Air sound speed =" + str(self.c0) + ", Air density =" + str(self.rho0)
