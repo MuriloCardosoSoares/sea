6d2003782ad62f0c0a7b5e688a522480870e695c
Murilo Cardoso Soares <58701063+MuriloCardosoSoares@users.noreply.github.com>
2020-11-23 11:25:46 -0300

    Update materials.py

--- sea/materials.py
+++ sea/materials.py
@@ -375,23 +375,24 @@ class Material():
                     1 : np.array([12.5,16,20,25,31.5,40,50,63,80,100,125,160,200,250,315,400,500,630,800,1000,1250,1600,2000,2500,3150,4000,5000,6300,8000,10000,12500,16000,20000])
                     }
 
-        data_in_bands = {}
 
-        aux = {0:0,
-               1:0}
-        upper_lim = upper[0]
+        first_band_aux = {0:0,
+                          1:0}
+        upper_limit = upper[0]
         while self.freq[0] > upper_limit[aux[0]]:
-            aux[0] = aux[0] + 1
+            first_band_aux[0] = first_band_aux[0] + 1
 
-        upper_lim = upper[1]
+        upper_limit = upper[1]
         while self.freq[0] > upper_limit[aux[1]]:
-            aux[1] = aux[1] + 1
+            first_band_aux[1] = first_band_aux[1] + 1
 
         for i in np.arange(len(upper)):
 
-            upper_lim = upper[i]
+            upper_limit = upper[i]
             center_freq = center[i]
-
+            aux = first_band_aux[i]
+            
+            data_in_bands = {}
             f_aux = 0
             for fi, f in enumerate(sel.freq):
 
