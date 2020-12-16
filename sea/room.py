a0a5ba6a423892f559a9735350a4aa8eda4bcfb7
Murilo Cardoso Soares <58701063+MuriloCardosoSoares@users.noreply.github.com>
2020-12-16 16:29:06 -0300

    Update room.py

--- sea/room.py
+++ sea/room.py
@@ -35,12 +35,12 @@ class Room:
         
     def air_properties(self, c0 = 343.0, rho0 = 1.21, temperature = 20.0, humid = 50.0, p_atm = 101325.0):
         self.air = Air(c0 = 343.0, rho0 = 1.21, temperature = 20.0, humid = 50.0, p_atm = 101325.0)
-        self.air.k0 = 2*np.pi*f/self.air.c0
+        self.air.k0 = 2*np.pi*self.frequencies.freq_vec/self.air.c0
         
     
     def algorithm_control(self, freq_init=20.0, freq_end=200.0, freq_step=1, freq_vec=[]):
         self.frequencies = Algorithm(freq_init, freq_end, freq_step, freq_vec)
-        self.air.k0 = 2*np.pi*f/self.air.c0
+        self.air.k0 = 2*np.pi*self.frequencies.freq_vec/self.air.c0
         
     
     def add_receiver(self, coord = [1.0, 0.0, 0.0]):
