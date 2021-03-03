e58a06be37a0456665e1005c16d3b6b9ba5db671
Murilo Cardoso Soares <58701063+MuriloCardosoSoares@users.noreply.github.com>
2021-03-03 18:11:45 -0300

    Update room.py

--- sea/room.py
+++ sea/room.py
@@ -688,6 +688,9 @@ class Room:
                                 plt.title("Room transfer function")
                                 plt.legend("Source %s, receiver %s" % (s_i, r_i))
                             else:
+                                print([item[0] for item in self.total_pressure[::len(self.frequencies.freq_vec)]])
+                                print([item[1] for item in self.total_pressure[::len(self.frequencies.freq_vec)]])
+                                
                                 plt.plot(self.frequencies.freq_vec, 20*np.log10(np.abs([item[0] for item in self.total_pressure[::len(self.frequencies.freq_vec)]])/2e-5))
                                 plt.plot(self.frequencies.freq_vec, 20*np.log10(np.abs([item[1] for item in self.total_pressure[::len(self.frequencies.freq_vec)]])/2e-5))
                                 plt.title("Binaural room transfer functions for source %s, receiver %s" % (s_i, r_i))
