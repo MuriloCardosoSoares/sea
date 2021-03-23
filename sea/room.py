222e014459b0f3d9edddc1f74a96220d8ff19622
Murilo Cardoso Soares <58701063+MuriloCardosoSoares@users.noreply.github.com>
2021-03-23 16:19:15 -0300

    Update room.py

--- sea/room.py
+++ sea/room.py
@@ -172,7 +172,11 @@ class Room:
         gmsh.finalize()
         '''
         max_element_size = (c0/freq)/6
-        os.system("gmsh -clmax $max_element_size -2 $self.path_to_geo -o last_msh.msh")
+        #os.system("gmsh -clmax $max_element_size -2 $self.path_to_geo -o last_msh.msh")
+        
+        from subprocess import check_call
+
+        check_call(["gmsh -clmax $max_element_size -2 $self.path_to_geo -o last_msh.msh"])
         
         '''
         #Reorder physical groups       
