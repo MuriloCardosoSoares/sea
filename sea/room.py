89b964b365893634eb3f00d873ac86ec278e5672
Murilo Cardoso Soares <58701063+MuriloCardosoSoares@users.noreply.github.com>
2021-05-05 09:20:45 -0300

    Update room.py

--- sea/room.py
+++ sea/room.py
@@ -773,12 +773,12 @@ class Room:
                         if s_i == source and r_i == receiver: 
                                                             
                             if r.type == "omni":
-                                plt.plot(self.simulated_freqs, np.unwrap(np.angle(self.total_pressure[s_i*len(self.receivers)+r_i : : len(self.sources)*len(self.receivers)]), deg=True), discont=360)
+                                plt.plot(self.simulated_freqs, np.unwrap(np.angle(self.total_pressure[s_i*len(self.receivers)+r_i : : len(self.sources)*len(self.receivers)], deg=True), discont=360))
                                 plt.title("Phase of the frequencie response")
                                 plt.legend(["Source %s, receiver %s" % (s_i, r_i)])
                             else:                                 
-                                plt.plot(self.simulated_freqs, np.unwrap(np.angle([item[0] for item in self.total_pressure[s_i*len(self.receivers)+r_i : : len(self.sources)*len(self.receivers)]]), deg=True), discont=180)
-                                plt.plot(self.simulated_freqs, np.unwrap(np.angle([item[1] for item in self.total_pressure[s_i*len(self.receivers)+r_i : : len(self.sources)*len(self.receivers)]]), deg=True), discont=180)
+                                plt.plot(self.simulated_freqs, np.unwrap(np.angle([item[0] for item in self.total_pressure[s_i*len(self.receivers)+r_i : : len(self.sources)*len(self.receivers)]], deg=True), discont=180))
+                                plt.plot(self.simulated_freqs, np.unwrap(np.angle([item[1] for item in self.total_pressure[s_i*len(self.receivers)+r_i : : len(self.sources)*len(self.receivers)]], deg=True), discont=180))
                                 plt.title("Binaural room transfer functions for source %s, receiver %s" % (s_i, r_i))
                                 plt.legend(["left", "right"])
 
