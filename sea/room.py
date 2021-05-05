5822a1ad63e3305f265bd152c5489de014f2a4de
Murilo Cardoso Soares <58701063+MuriloCardosoSoares@users.noreply.github.com>
2021-05-05 10:42:53 -0300

    Update room.py

--- sea/room.py
+++ sea/room.py
@@ -774,13 +774,13 @@ class Room:
                                                             
                             if r.type == "omni":
                                 plt.plot(self.simulated_freqs, np.unwrap(np.angle(self.total_pressure[s_i*len(self.receivers)+r_i : : len(self.sources)*len(self.receivers)], deg=True), discont=360))
-                                plt.xticks(np.arange(-180,210,30))
+                                plt.xticks(np.arange(-180,210,30), np.arange(-180,210,30))
                                 plt.title("Phase of the frequencie response")
                                 plt.legend(["Source %s, receiver %s" % (s_i, r_i)])
                             else:                                 
                                 plt.plot(self.simulated_freqs, np.unwrap(np.angle([item[0] for item in self.total_pressure[s_i*len(self.receivers)+r_i : : len(self.sources)*len(self.receivers)]], deg=True), discont=180))
                                 plt.plot(self.simulated_freqs, np.unwrap(np.angle([item[1] for item in self.total_pressure[s_i*len(self.receivers)+r_i : : len(self.sources)*len(self.receivers)]], deg=True), discont=180))
-                                plt.xticks(np.arange(-180,210,30))
+                                plt.xticks(np.arange(-180,210,30), np.arange(-180,210,30))
                                 plt.title("Binaural room transfer functions for source %s, receiver %s" % (s_i, r_i))
                                 plt.legend(["left", "right"])
 
