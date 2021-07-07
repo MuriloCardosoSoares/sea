10e4118d2aacdcb9674a18d0a07293db2a820aae
Murilo Cardoso Soares <58701063+MuriloCardosoSoares@users.noreply.github.com>
2021-07-07 12:04:27 -0300

    Update post.py

--- sea/post.py
+++ sea/post.py
@@ -23,39 +23,38 @@ def mac (reference_path, compared_paths, sources=[], receivers=[], plot=True):
     ref = pickle.load(file_to_read)
     file_to_read.close()
     
-    compared = []
+    compared_list = []
     for i in enumerate(compared_paths):
         file_to_read = open(compared_paths[i], "rb")
         compared.append(pickle.load(file_to_read))
         file_to_read.close()
     
-    for source in enumerate(sources):
-        to_be_compared = []
-        reference = []
-        for c_i in np.arange(num_materials_configurations):
-            for s_i in np.arange(num_sources):
-                for r_i in np.arange(num_receivers):
-                    if c_i == ref-1 and s_i == source_analisys-1:
-                        reference[i] = total_pressure_receivers[i,:]
-                    if c_i != ref-1 and s_i == source_analisys-1:
-                        to_be_compared[i] = total_pressure_receivers[i,:]
+    mac_list = []
+    for source in sources:
+        for compared in compared_list:
+ 
+            reference = []
+            to_be_compared = []
+            
+            for receiver in receivers:
 
-        trash, reference =  zip(*sorted(reference.items()))
-        reference = np.array(reference)
-
-        trash, to_be_compared =  zip(*sorted(to_be_compared.items()))
-        to_be_compared = np.array(to_be_compared)
+                for s_i, s in enumerate(compared[0].sources):
+                    for r_i, r in enumerate(compared[0].receivers):
+                        if s_i == source and r_i == receiver:
+                            reference.append([ref.total_pressure[s_i*len(ref.receivers)+r_i : : len(ref.sources)*len(ref.receivers)]])
+                            to_be_compared.append([compared.total_pressure[s_i*len(compared.receivers)+r_i : : len(compared.sources)*len(compared.receivers)]])
 
 
-        mac = np.zeros((num_materials_configurations - 1, np.size(f_range)))
-        for i in np.arange(num_materials_configurations - 1):
+            mac = []
 
-            for fi, f in enumerate(f_range):
+            for fi, f in enumerate(reference[0].frequencies.freq_vec):
 
-                ref_aux = reference[:,fi]
+                ref_aux = np.array(reference)[:,fi]
                 to_be_compared_aux = to_be_compared[i*(num_receivers) : i*(num_receivers)+num_receivers, fi]
-                mac[i,fi] = (abs(np.matmul(ref_aux.conj(), to_be_compared_aux.transpose()))**2) / np.real((np.matmul(ref_aux.conj(), ref_aux.transpose())) * np.matmul(to_be_compared_aux.conj(), to_be_compared_aux.transpose()))
+                mac.append((abs(np.matmul(ref_aux.conj(), to_be_compared_aux.transpose()))**2) / np.real((np.matmul(ref_aux.conj(), ref_aux.transpose())) * np.matmul(to_be_compared_aux.conj(), to_be_compared_aux.transpose())))
 
+            mac_list.append([mac])
+'''              
     for configuration_i in np.arange(num_materials_configurations-1): 
         
         plt.plot(f_range, mac[configuration_i, :])
@@ -76,3 +75,7 @@ def mac (reference_path, compared_paths, sources=[], receivers=[], plot=True):
         fh.create_dataset("mac", data=mac)
 
         print ("Saved results to %s" % file_path)
+'''
+    mac_list = np.array([mac_list])
+    
+    return mac_list
