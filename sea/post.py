a96246c26839b6964a4a694fa162ae5745100a03
Murilo Cardoso Soares <58701063+MuriloCardosoSoares@users.noreply.github.com>
2021-07-07 13:24:25 -0300

    Update post.py

--- sea/post.py
+++ sea/post.py
@@ -51,7 +51,7 @@ def mac (reference_path, compared_paths, sources=[], receivers=[], plot=True):
                         print(s_i)
                         print(source)
                         print(r_i)
-                        print(reeiver)
+                        print(receiver)
                         if s_i == source and r_i == receiver:
                             reference.append([ref.total_pressure[s_i*len(ref.receivers)+r_i : : len(ref.sources)*len(ref.receivers)]])
                             to_be_compared.append([compared.total_pressure[s_i*len(compared.receivers)+r_i : : len(compared.sources)*len(compared.receivers)]])
