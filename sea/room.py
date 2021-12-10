ed18ec6d15667d034d821f87ef5de7ea61544c27
Murilo Cardoso Soares <58701063+MuriloCardosoSoares@users.noreply.github.com>
2021-12-10 17:39:05 -0300

    Update room.py

--- sea/room.py
+++ sea/room.py
@@ -960,7 +960,7 @@ class Room:
             
             for i, element in enumerate(elements):  
                 color = 'rgb(%s,%s,%s)' % (color_list_rgb[i][0], color_list_rgb[i][1], color_list_rgb[i][2])
-                fig.add_trace(go.Scatter3d(x=[x[elements[0][i]]], y=[y[elements[1][i]]], z=[zelements[2][i]]], marker=dict(size=10, color=color, symbol='diamond'), showlegend=False)) 
+                fig.add_trace(go.Scatter3d(x=[x[elements[0][i]]], y=[y[elements[1][i]]], z=[z[elements[2][i]]], marker=dict(size=10, color=color, symbol='diamond'), showlegend=False)) 
 
             fig.add_trace(go.Mesh3d(x=[-6,6,-6,6], y=[-6,6,-6,6], z=0 * np.zeros_like([-6,6,-6,6]), color='red', opacity=0.5, showscale=False))
 
